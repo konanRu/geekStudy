2bc2f927f4d24d9b0ea9175f23731fd3adb71c57
XAH <konan_ru@mail.ru>
2020-07-01 10:00:13 +0300

    Home Work Lessons4

--- JS HomeWork/script.js
+++ JS HomeWork/script.js
@@ -1,99 +1,129 @@
 'use strict';
 
-
 /**
- * С помощью цикла while вывести все простые числа в промежутке от 0 до 100.
+ * Сделайте в стиле es5, а затем в стиле es6, конструктор Product, который принимает параметры name и price, сохраните их как свойства объекта. Также объекты типа Product должны иметь метод make25PercentDiscount, который будет уменьшать цену в объекте на 25%.
  */
-function task1() {
-    /**
-     * возвращает true если аргумент является простым числом, иначе false/
-     * @param {*} num - число для проверки на свойство "простоты"
-     */
-    function chek_easy_number(num) {
-        if (easy_numbers.length > 0) {
-            for (let easy_number of easy_numbers) {
-                if (num % easy_number === 0) {
-                    return false;
-                }
-            }
-        }
-        easy_numbers.push(num);
-        return true;
+function Task1_1() {
+
+    function Product_es5(name, price) { //в стиле es5
+        this.name = name;
+        this.price = price;
+    }
+
+    Product_es5.prototype.make25PercentDiscount = function () {
+        this.price = this.price * 0.75;
+        return '-25% OFF';
     };
 
-    let easy_numbers = [];
-    let i = 1;
-    while (i < 100) {
-        i++;
-        if (chek_easy_number(i)) {
-            console.log(i);
+    class Product_es6 { // стиле es6
+        constructor(name, price) {
+            this.name = name;
+            this.price = price;
+        }
+        make25PercentDiscount() {
+            this.price = this.price * 0.75;
+            return '-25% OFF';
+        }
+
+    }
+
+    const product1_es5 = new Product_es5('moto1', 200000);
+    const product1_es6 = new Product_es6('moto2', 100000);
+    console.log('Task 1 1 ==>');
+
+    console.log(product1_es5);
+    console.log(product1_es5.make25PercentDiscount());
+    console.log(product1_es5);
+
+    console.log(product1_es6);
+    console.log(product1_es6.make25PercentDiscount());
+    console.log(product1_es6);
+
+    console.log('<==Task 1 1.');
+}
+
+/**
+ * Сделайте в стиле es5, а затем в стиле es6 (по аналогии из урока),
+а) конструктор Post, который принимает параметры author, text, date и сохраняет их как свойства объекта. Объекты типа Post должны иметь метод edit, который будет принимать текст и записывать его в свойство text объекта.
+б) конструктор AttachedPost, который принимает параметры author, text, date. Проинициализируйте эти свойства с помощью конструктора Post, чтобы не дублировать код. Также в конструкторе AttachedPost должно создаваться свойство highlighted со значением false. Унаследуйте в объектах типа AttachedPost методы из Post.
+Объекты типа AttachedPost должны иметь метод makeTextHighlighted, который будет назначать свойству highlighted значение true.
+*/
+function Task1_2() {
+    function Post_es5(author, text, date) {
+        this.author = author;
+        this.text = text;
+        this.date = date;
+    }
+
+    Post_es5.prototype.edit = function (text) {
+        this.text = text;
+        return 'edited';
+    };
+
+    function AttachedPost_es5(author, text, date) {
+        Post_es5.call(this, author, text, date);
+        this.highlighted = false;
+    }
+
+    AttachedPost_es5.prototype = Object.create(Post_es5.prototype);
+    AttachedPost_es5.prototype.constructor = AttachedPost_es5;
+    AttachedPost_es5.prototype.makeTextHighlighted = function () {
+        this.highlighted = true;
+        return 'highlighted';
+    };
+
+    class Post_es6 {
+        constructor(author, text, date) {
+            this.author = author;
+            this.text = text;
+            this.date = date;
+        }
+
+        edit(text) {
+            this.text = text;
+            return 'edited';
         }
     }
-}
-
-/**
- * 3. Товары в корзине хранятся в массиве. Задачи:
- * a) Организовать такой массив для хранения товаров в корзине;
- * b) Организовать функцию countBasketPrice, которая будет считать стоимость корзины.
- */
-// function task3() {
-
-/**
- * Возвращает сумму товаров, находящихся в корзине покупок.
- * @param {*} shopping_cart - массив с данными корзины покупок.
- */
-function getSumCart(shopping_cart) {
-    let amountOfPurchase = 0;
-    if (shopping_cart.length > 0) {
-        for (let productInCart of shopping_cart) {
-            amountOfPurchase = amountOfPurchase + (productInCart.quantity * productInCart.price);
+    class AttachedPost_es6 extends Post_es6 {
+        constructor(author, text, date) {
+            super(author, text, date);
+            this.highlighted = false;
         }
-        return amountOfPurchase;
-    }
-    return 'Корзина пуста!';
-}
 
-let shopping_cart = [
-    {
-        article: 5,
-        quantity: 27,
-        price: 5
-    },
-    {
-        article: 9,
-        quantity: 15,
-        price: 75
-    }
-];
-console.log(getSumCart(shopping_cart))
-// }
-
-/**
- * Вывести с помощью цикла for числа от 0 до 9, не используя тело цикла. 
- */
-function task4() {
-    let j = 0;
-    let i = 0;
-    while (j < 9) {
-        ++j;
-        for (; i < j; i++) {}
-        console.log(i);
-    }
-
-}
-
-/**
- * Нарисовать пирамиду с помощью console.log, как показано на рисунке, только у вашей пирамиды должно быть 20 рядов, а не 5:
- */
-function task5() {
-    let x_arr = [];
-    let x_string = "";
-    for (let j = 1; j < 21; j++) {
-        for (let i = 0; i < j; i++) {
-            x_string = x_string + 'x';
+        makeTextHighlighted() {
+            this.highlighted = true;
+            return 'highlighted';
         }
-        x_arr.push(x_string);
-        x_string = '';
     }
-    console.log(x_arr);
+
+    const post1_es5 = new Post_es5('Alex', 'привет!', '2014');
+    const AttachedPost1_es5 = new AttachedPost_es5('Dim', 'не привет!', '2015');
+    const post1_es6 = new Post_es6('Jhon', 'здрасти Вам!', '2014');
+    const AttachedPost1_es6 = new AttachedPost_es6('Tom', 'Ок!', '2015');
+
+    console.log('Task 1 2 ==>');
+
+    console.log(post1_es5);
+    console.log(post1_es5.edit('Хорошо'));
+    console.log(post1_es5);
+
+    console.log(AttachedPost1_es5);
+    console.log(AttachedPost1_es5.edit('Бывай'));
+    console.log(AttachedPost1_es5.makeTextHighlighted());
+    console.log(AttachedPost1_es5);
+
+    console.log(post1_es6);
+    console.log(post1_es6.edit('Ок'));
+    console.log(post1_es6);
+
+    console.log(AttachedPost1_es6);
+    console.log(AttachedPost1_es6.edit('Окей!'));
+    console.log(AttachedPost1_es6.makeTextHighlighted());
+    console.log(AttachedPost1_es6);
+
+    console.log('<==Task 1 2.');
+
 }
+
+Task1_1()
+Task1_2()
